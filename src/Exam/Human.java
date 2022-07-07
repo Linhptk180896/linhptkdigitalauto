6c002d07ad92d1b07d86049d2811a3bbe246f711
linhptk <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>
2022-07-07 16:46:52 +0700

    no message

--- src/Exam/Human.java
+++ src/Exam/Human.java
@@ -12,7 +12,7 @@ public class Human {
     public String getFirstName() {
         return firstName;
     }
-//dd
+//dd commit  test
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
